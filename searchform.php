40d1c6458e93739c2ed78009c64168c94cd06b9e
sarahellisfox <sarah.ellis.fox@gmail.com>
2019-11-22 14:09:50 -0800

    style 404 page

--- searchform.php
+++ searchform.php
@@ -4,7 +4,7 @@
 			<input type="search" class="search-field" placeholder="SEARCH ..." value="<?php echo esc_attr( get_search_query() ); ?>" name="s" title="Search for:" />
 		</label>
 		<button class="search-submit">
-			<?php echo esc_html( 'Search' ); ?>
+			<i id="searchIcon" class="fas fa-search-plus"></i>
 		</button>
 	</fieldset>
 </form>
